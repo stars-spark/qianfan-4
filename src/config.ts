d103c19a9a02f284d6cb4780688068c48a936a16
Stars <53867496+stars-spark@users.noreply.github.com>
2024-08-25 18:52:58 +0800

    change github

--- src/config.ts
+++ src/config.ts
@@ -66,7 +66,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/stars-spark/qianfan-4',
     },
   ],
 }
